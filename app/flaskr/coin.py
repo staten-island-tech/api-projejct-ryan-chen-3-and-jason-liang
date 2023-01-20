27553b7b5ed328dd7b07b844653ebb42044d649e
Shenixx <113193025+Shenixx@users.noreply.github.com>
2023-01-19 20:34:24 -0500

    uh

--- app/flaskr/coin.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-url = 'https://rest.coinapi.io/v1/assets'
-headers = {'X-CoinAPI-Key' : '411BC328-3684-44CB-AC37-0200E00B2152'}
-response = requests.get(url, headers=headers)
